26eaacfef52c81063bc0692eb48eec7b94d18ce1
Samarth <write2samartha@gmail.com>
2025-09-09 21:21:11 -0700

    Added instructer arjun

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <main style={{ padding: "2rem" }}>
       <h1>About</h1>
-      <p>(Coming soon! Classes by Samarth)</p>
+      <p>(Coming soon! Classes by Samarth and Arjun)</p>
     </main>
   );
 }
