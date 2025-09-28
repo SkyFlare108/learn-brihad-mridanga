8af83d2d412bdb2b1c768a80359cbef4515450ef
Yerramsetti, Harisri <hari.yerrams@gmail.com>
2025-09-27 20:57:43 -0400

    Change

--- src/components/InstructorSlideshow.tsx
+++ src/components/InstructorSlideshow.tsx
@@ -1,9 +1,9 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import samarth from "my-site/src/assets/samarth-photo.png";
-import arjun from "my-site/src/assets/arjun-photo.png";
-import hari from "my-site/src/assets/hari-photo.png";  
+import samarth from "../assets/samarth-photo.png";
+import arjun from "../assets/arjun-photo.png";
+import hari from "../assets/hari-photo.png";  
 
 const instructors = [
   {
